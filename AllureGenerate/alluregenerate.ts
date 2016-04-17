045bc3cf560a9793f99a7ef56e93895ac678abdd
just-boris <just-boris@hotmail.com>
2016-04-17 15:20:59 +0300

    bundle allure

--- AllureGenerate/alluregenerate.ts
+++ AllureGenerate/alluregenerate.ts
@@ -1,17 +1,24 @@
 /// <reference path="../node_modules/Agent.Tasks/definitions/vsts-task-lib.d.ts" />
 
 import tl = require('vsts-task-lib/task');
+import path = require('path');
 
-var allureRunner = tl.getInput('allureRunner', true);
+var allureRunner = tl.getInput('allureRunner', false);
+if(!allureRunner) {
+    allureRunner = require.resolve('allure-commadline/bin/allure');
+}
 var resultsDir = tl.getInput('resultsDir', true);
 var targetDir = tl.getInput('targetDir', true);
+var nodePath = tl.which('node', true);
 
 tl.debug('allureRunner: ' + allureRunner);
 tl.debug('resultsDir: ' + resultsDir);
 tl.debug('targetDir: ' + targetDir);
+tl.debug('nodePath: ' + nodePath);
 
-var allure = tl.createToolRunner(allureRunner);
+var allure = tl.createToolRunner(nodePath);
 
+allure.argString(allureRunner);
 allure.argString('generate');
 allure.argString('--output ' + targetDir);
 allure.argString(resultsDir);
